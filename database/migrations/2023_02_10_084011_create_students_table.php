eea4cb6f348ec0670446ae20fd5f7c3485ed6662
IngridB-afk <ingridbarrachina@gmail.com>
2023-02-10 14:30:43 +0100

    Created factorie, model and students table/Created the translation of the validation messages cat/es

--- database/migrations/2023_02_10_084011_create_students_table.php
+++ database/migrations/2023_02_10_084011_create_students_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('locations', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('location');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('phone');
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('locations');
+        Schema::dropIfExists('students');
     }
 };
