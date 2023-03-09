d0256481c72505021324ca0aa098b98a6a7acbfe
Kuantize Strigoi <adrian.sunye@gmail.com>
2023-03-09 08:55:11 +0100

    fixed middleware fore store users function

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -10,7 +10,7 @@ class StudentController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api', ['except' => ['store']]);
     }
     /**
      * Display a listing of the resource.
